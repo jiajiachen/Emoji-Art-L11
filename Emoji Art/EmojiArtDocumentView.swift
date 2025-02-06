ccaa89c38e8342f027a6a7255836af07c1c2bb8c
KellyChen <matijueming@gmail.com>
2025-02-06 20:59:43 +0800

    Initial Commit

---
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -0,0 +1,145 @@
+//
+//  EmojiArtDocumentView.swift
+//  Emoji Art
+//
+//  Created by CS193p Instructor on 5/8/23.
+//  Copyright (c) 2023 Stanford University
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    typealias Emoji = EmojiArt.Emoji
+    
+    @ObservedObject var document: EmojiArtDocument
+    
+    private let paletteEmojiSize: CGFloat = 40
+
+    var body: some View {
+        VStack(spacing: 0) {
+            documentBody
+            PaletteChooser()
+                .font(.system(size: paletteEmojiSize))
+                .padding(.horizontal)
+                .scrollIndicators(.hidden)
+        }
+    }
+    
+    private var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.white
+                documentContents(in: geometry)
+                    .scaleEffect(zoom * gestureZoom)
+                    .offset(pan + gesturePan)
+            }
+            .gesture(panGesture.simultaneously(with: zoomGesture))
+            .dropDestination(for: Sturldata.self) { sturldatas, location in
+                return drop(sturldatas, at: location, in: geometry)
+            }
+            .onTapGesture {
+                unSelectAllEmojis()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func documentContents(in geometry: GeometryProxy) -> some View {
+        AsyncImage(url: document.background)
+            .position(Emoji.Position.zero.in(geometry))
+        ForEach(document.emojis) { emoji in
+            Text(emoji.string)
+                .font(emoji.font)
+                .border(Color.red.opacity(isSelected(emoji) ? 1 : 0), width: 3)
+                .position(emoji.position.in(geometry))
+                .onTapGesture {
+                    if isSelected(emoji) {
+                        unSelectEmoji(emoji)
+                    } else {
+                        selectEmoji(emoji)
+                    }
+                }
+                
+        }
+    }
+
+    @State private var zoom: CGFloat = 1
+    @State private var pan: CGOffset = .zero
+    
+    @GestureState private var gestureZoom: CGFloat = 1
+    @GestureState private var gesturePan: CGOffset = .zero
+    
+    
+    
+    private var zoomGesture: some Gesture {
+        MagnificationGesture()
+            .updating($gestureZoom) { inMotionPinchScale, gestureZoom, _ in
+                gestureZoom = inMotionPinchScale
+            }
+            .onEnded { endingPinchScale in
+                zoom *= endingPinchScale
+            }
+    }
+    
+    private var panGesture: some Gesture {
+        DragGesture()
+            .updating($gesturePan) { inMotionDragGestureValue, gesturePan, _ in
+                gesturePan = inMotionDragGestureValue.translation
+            }
+            .onEnded { endingDragGestureValue in
+                pan += endingDragGestureValue.translation
+            }
+    }
+    
+    @State private var selectEmojiList = Set<Emoji.ID>()
+    
+    private func selectEmoji(_ emoji: Emoji) {
+        selectEmojiList.insert(emoji.id)
+    }
+    private func isSelected(_ emoji: Emoji) -> Bool {
+        selectEmojiList.contains(emoji.id)
+    }
+    
+    private func unSelectEmoji(_ emoji: Emoji) {
+        selectEmojiList.remove(emoji.id)
+    }
+    
+    private func unSelectAllEmojis() {
+        selectEmojiList.removeAll()
+    }
+    
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturldata in sturldatas {
+            switch sturldata {
+            case .url(let url):
+                document.setBackground(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(
+                    emoji,
+                    at: emojiPosition(at: location, in: geometry),
+                    size: paletteEmojiSize / zoom
+                )
+                return true
+            default:
+                break
+            }
+        }
+        return false
+    }
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int((location.x - center.x - pan.width) / zoom),
+            y: Int(-(location.y - center.y - pan.height) / zoom)
+        )
+    }
+}
+
+struct EmojiArtDocumentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentView(document: EmojiArtDocument())
+            .environmentObject(PaletteStore(named: "Preview"))
+    }
+}
